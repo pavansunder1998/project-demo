e226d18159e4c0b88ec0cb6b34f61fa30942d5a0
pavan sunder <63669695+pavansunder1998@users.noreply.github.com>
2023-08-31 15:52:24 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data1.csv')
-country_name = input('Enter the country name: ')
-rowi = df[df['Country'] == country_name].index[0]
-
-print('The Job Satisfaction for '+ country_name+' is ' +str(df.loc[rowi, 'JobSatisfaction']))
