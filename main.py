ee737eb7135163ba83b62357cfcac5382c568079
pavan sunder <63669695+pavansunder1998@users.noreply.github.com>
2023-09-01 09:13:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import pandas as pd
 
-df = pd.read_csv('data1.csv')
-country_name = input('Enter the country name: ')
-rowi = df[df['Country'] == country_name].index[0]
 
-print('The Job Satisfaction for '+ country_name+' is ' +str(df.loc[rowi, 'JobSatisfaction']))
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+
+def get_job_satisfaction_counts(data_frame, country_name):
+    matching_rows = data_frame[data_frame['Country'] == country_name]
+    satisfaction_counts = matching_rows['JobSatisfaction'].value_counts()
+    return satisfaction_counts
+
+
+def main():
+    data_file = 'data3.csv'
+    df = load_data(data_file)
+
+    while True:
+        country_name = input('Enter the country name (or type "exit" to quit): ')
+
+        if country_name.lower() == 'exit':
+            print('Exiting...')
+            break
+
+        satisfaction_counts = get_job_satisfaction_counts(df, country_name)
+
+        if not satisfaction_counts.empty:
+            print(f'Job Satisfaction distribution for {country_name}:')
+            for satisfaction, count in satisfaction_counts.items():
+                print(f'{satisfaction}: {count} people')
+        else:
+            print(f'No data found for {country_name}')
+
+
+if __name__ == "__main__":
+    main()
